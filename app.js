33c47ab0e1952b2a1e7fd6957797cc920060472c
shankhanil007 <shankhanilborthakur@gmail.com>
2021-10-09 18:58:20 +0530

    feat: initialising passport

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 var path = require("path");
 const express = require("express");
-
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const passportLocalMongoose = require("passport-local-mongoose");
 const app = express();
 
 app.use(express.static("public"));
+
+//------------- Initialising passport ----------------
+
+app.use(
+  require("express-session")({
+    secret: "This the secret message for authentication",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 // Landing page
 app.get("/", function (req, res) {
